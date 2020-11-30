c522b34b77262dd1ee0cdf827f5f9b89e68b8d2b
Rahul Sahay <rahulsahay19@hotmail.com>
2020-11-30 20:58:53 +0530

    removed redundant code.

--- Movies.Application/Mappers/MovieMapper.cs
+++ Movies.Application/Mappers/MovieMapper.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using AutoMapper;
 
 namespace Movies.Application.Mappers
@@ -11,7 +9,6 @@ namespace Movies.Application.Mappers
         {
             var config = new MapperConfiguration(cfg =>
             {
-                // This line ensures that internal properties are also mapped over.
                 cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
                 cfg.AddProfile<MovieMappingProfile>();
             });
